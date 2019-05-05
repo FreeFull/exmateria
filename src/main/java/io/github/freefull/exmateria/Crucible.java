da91373bbefbee0a680313bceeeb6c8c7b41b328
Filip Szczepański <jazz2rulez@gmail.com>
2019-05-05 16:38:44 +0100

    Initial crucible implementation

---
+++ src/main/java/io/github/freefull/exmateria/Crucible.java
@@ -0,0 +1,9 @@
+package io.github.freefull.exmateria;
+
+import net.minecraft.block.Block;
+
+public class Crucible extends Block {
+    public Crucible(Settings settings) {
+        super(settings);
+    }
+}
--- src/main/java/io/github/freefull/exmateria/ExMateria.java
+++ src/main/java/io/github/freefull/exmateria/ExMateria.java
@@ -1,14 +1,25 @@
 package io.github.freefull.exmateria;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ExMateria implements ModInitializer {
+	public static final Block CRUCIBLE = new Crucible(FabricBlockSettings.of(Material.METAL).build());
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		Registry.register(Registry.BLOCK, new Identifier("exmateria", "crucible"), CRUCIBLE);
+		Registry.register(Registry.ITEM, new Identifier("exmateria", "crucible"), new BlockItem(CRUCIBLE, new Item.Settings().itemGroup(ItemGroup.MISC)));
 	}
 }
